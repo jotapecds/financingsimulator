88037d86062f3c94bd70dc62d2637ede66f9eb96
João Pedro Souza <52897367+jotapecds@users.noreply.github.com>
2023-12-11 22:24:37 -0300

    testing changes

--- api/index.php
+++ api/index.php
@@ -18,25 +18,19 @@
     <div id="details-container">
         <h3 id="details-header"><span>&#8688;</span>Informações detalhadas</h3>
         <ul id="details-list" style="display: none;">
-            <li>Parcelas: {$NP}</li>
-            <li>Taxa de juros: {$TAX}%</li>
-            <li>Valor Financiado: {$PV}</li>
-            <li>Valor a voltar: {$PP}</li>
-            <li>Meses a voltar: {$PB}</li>
-            <li>Entrada(?): {$existe_entrada}</li>
-        <?php echo $existe_entrada;
+        <?php
             // $cf = calculateFinancingCoefficient($TAX, $NP);
             // $cf_rouded = round($cf);
             // $prest = roud($PV*$cf);
             // $tx_real = calculateInterestRate($NP,$PV,$PP,$existe_entrada) * 100;
 
-            // echo "<li>Parcelas: {$NP}</li>
-            //         <li>Taxa de juros: {$TAX}%</li>
-            //         <li>Valor Financiado: {$PV}</li>
-            //         <li>Valor a voltar: {$PP}</li>
-            //         <li>Meses a voltar: {$PB}</li>
-            //         <li>Entrada(?): {$existe_entrada}</li>
-            //         <br>
+            echo "<li>Parcelas: {$NP}</li>
+                    <li>Taxa de juros: {$TAX}%</li>
+                    <li>Valor Financiado: {$PV}</li>
+                    <li>Valor a voltar: {$PP}</li>
+                    <li>Meses a voltar: {$PB}</li>
+                    <li>Entrada(?): {$existe_entrada}</li>
+                    <br>";
             //         <li>Coeficiente de Financiamento: {$cf_rouded}</li>
             //         <li>Prestação: {$prest}</li>
             //         <li>Valor pago: </li>
